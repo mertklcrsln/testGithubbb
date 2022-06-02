9f4263d4c9000907fee18e6a93f3c126e199647b
Sezayi Aslan <sezayi.aslan@partner.turktelekom.com.tr>
2022-06-03 00:14:14 +0300

    expected fail test

--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -12,4 +12,13 @@ public class MathTest {
         int result= math.add(a,b);
         assertEquals(result,expected);
     }
+    @Test
+    public void addTests(){
+        int a=5;
+        int b=4;
+        int expected=10;
+        Math math= new Math();
+        int result= math.add(a,b);
+        assertEquals(result,expected);
+    }
 }
