0dc182b1235a8f0d26048bb923a7f14a03d58098
Sezayi Aslan <sezayi.aslan@partner.turktelekom.com.tr>
2022-06-02 23:48:19 +0300

    Initial commit

---
+++ src/test/java/MathTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MathTest {
+    @Test
+    public void addTest(){
+        int a=5;
+        int b=4;
+        int expected=9;
+        Math math= new Math();
+        int result= math.add(a,b);
+        assertEquals(result,expected);
+    }
+}
