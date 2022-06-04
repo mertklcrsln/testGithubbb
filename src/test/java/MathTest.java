3725e1d9bd698b80eddc7424979577252ce9d19e
Sezayi Aslan <sezayi.aslan@partner.turktelekom.com.tr>
2022-06-04 13:09:24 +0300

    deneme

--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -16,7 +16,7 @@ public class MathTest {
     public void addTests(){
         int a=5;
         int b=4;
-        int expected=10;
+        int expected=9;
         Math math= new Math();
         int result= math.add(a,b);
         assertEquals(result,expected);
