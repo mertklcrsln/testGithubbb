37d290251ed1a59ee739a01c4f731c119e38da3c
mertklcrsln <mertklcrsln@gmail.com>
2022-06-24 05:22:08 +0000

    update

--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathTest {
     @Test
-    public void addTest(){
+    public void besArtiDortEsittirDokuzTesti1(){
         int a=5;
         int b=4;
         int expected=9;
@@ -13,10 +13,37 @@ public class MathTest {
         assertEquals(result,expected);
     }
     @Test
-    public void addTests(){
+    public void besArtiDortEsittirDokuzTesti2(){
         int a=5;
         int b=4;
         int expected=9;
+        Math math= new Math();
+        int result= math.add(a,b);
+        assertEquals(result,expected);
+    }
+    @Test
+    public void besArtiDortEsittirDokuzTesti3(){
+        int a=5;
+        int b=4;
+        int expected=9;
+        Math math= new Math();
+        int result= math.add(a,b);
+        assertEquals(result,expected);
+    }
+    @Test
+    public void besArtiDortEsittirOnTesti(){
+        int a=5;
+        int b=4;
+        int expected=10;
+        Math math= new Math();
+        int result= math.add(a,b);
+        assertEquals(result,expected);
+    }
+    @Test
+    public void besArtiDortEsittirYirmiTesti(){
+        int a=5;
+        int b=4;
+        int expected=20;
         Math math= new Math();
         int result= math.add(a,b);
         assertEquals(result,expected);
